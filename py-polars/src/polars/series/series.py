afd25e1378dbf2e35c734832fd9001dc1eb38e32
Amber Sprenkels <amber@polars.tech>
2026-07-15 11:36:36 +0200

    Fix feature chain

--- py-polars/src/polars/series/series.py
+++ py-polars/src/polars/series/series.py
@@ -9878,7 +9878,7 @@ class Series:
     def approx_quantile(
         self, quantile: float | list[float], error: 1 / 1_000_000
     ) -> PythonLiteral:
-        (
+        return (
             self.to_frame()
             .select_seq(F.col(self.name).approx_quantile(quantile, error=error))
             .to_series()
